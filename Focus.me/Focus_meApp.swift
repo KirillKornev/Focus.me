995d6aa1a4a2f128d0d857de1f9e10d9bd725e18
Kirill Kornev <kirill_kornew@mail.ru>
2022-12-01 20:49:31 +0400

    Initial commit

---
+++ Focus.me/Focus_meApp.swift
@@ -0,0 +1,17 @@
+//
+//  Focus_meApp.swift
+//  Focus.me
+//
+//  Created by Kirill Kornev on 01.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct Focus_meApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
