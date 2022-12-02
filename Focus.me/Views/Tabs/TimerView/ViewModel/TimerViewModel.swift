a69cdb4eae034b1bbc2d99ed582929a45488d2ba
Kirill Kornev <kirill_kornew@mail.ru>
2022-12-02 18:54:12 +0400

    UI changes

--- Focus.me/Views/Tabs/TimerView/ViewModel/TimerViewModel.swift
+++ Focus.me/Views/Tabs/TimerView/ViewModel/TimerViewModel.swift
@@ -10,11 +10,17 @@ import SwiftUI
 final class TimerViewModel: ObservableObject {
 
     @Published var isRunning = false
+    @Published var isStopped = false
     @Published var timeRemaining: Int = .zero
     @Published var fullTimePeriod: AvailableTime = .one
 
     func setNewPeriod(_ period: AvailableTime) {
         timeRemaining = period.inSeconds
         fullTimePeriod = period
+    }
+
+    func stop() {
+        timeRemaining = fullTimePeriod.inSeconds
+        isRunning = false
     }
 }
--- Focus.me/Views/Tabs/TimerView/Views/TimerButton.swift
+++ Focus.me/Views/Tabs/TimerView/Views/TimerButton.swift
@@ -13,16 +13,25 @@ struct TimerButton: View {
     @EnvironmentObject var vm: TimerViewModel
 
     var body: some View {
-        Button {
-            vm.isRunning.toggle()
-        } label: {
-            if vm.isRunning {
-                Text("Pause")
-                    .padding(20)
-            } else {
-                Text("Run")
-                    .padding(20)
+        VStack {
+            Button {
+                vm.isRunning.toggle()
+            } label: {
+                if vm.isRunning {
+                    Text("Pause")
+                        .padding(20)
+                } else {
+                    Text("Run")
+                        .padding(20)
+                }
             }
+            .contentShape(Rectangle())
+        }
+
+        Button {
+            vm.stop()
+        } label: {
+            Text("Stop")
         }
         .contentShape(Rectangle())
     }
