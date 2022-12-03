9d0f4294305a76603bb73ee0d56204aa3b95e507
Kirill Kornev <kirill_kornew@mail.ru>
2022-12-03 21:31:20 +0400

    Added tabView

--- Focus.me/Focus_meApp.swift
+++ Focus.me/Focus_meApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Focus_meApp: App {
     var body: some Scene {
         WindowGroup {
-            MainTimerView()
+            MainView()
         }
     }
 }
---
+++ Focus.me/Views/Tabs/MainView.swift
@@ -0,0 +1,51 @@
+//
+//  MainView.swift
+//  Focus.me
+//
+//  Created by Kirill Kornev on 03.12.2022.
+//
+
+import SwiftUI
+
+private extension CGFloat {
+    static let minDragTranslationForSwipe: CGFloat = 50
+}
+
+struct MainView: View {
+
+    @State private var selectedTab: Int = .zero
+
+    // Computed
+    private var numTabs: Int {
+        return 2
+    }
+    
+    var body: some View {
+        TabView(selection: $selectedTab){
+            NavigationView{
+                MainTimerView()
+            }.tabItem {
+                Image(systemName: "timer")
+//                Text("Home")
+            }.tag(0)
+                .highPriorityGesture(DragGesture().onEnded({ self.handleSwipe(translation: $0.translation.width)}))
+            NavigationView{
+                Text("Salut, tout le monde!")
+            }.tabItem {
+                Image(systemName: "timelapse")
+//                Text("Space")
+            }.tag(1)
+                .highPriorityGesture(DragGesture().onEnded({ self.handleSwipe(translation: $0.translation.width)}))
+        }
+    }
+
+    // MARK: - Private
+
+    private func handleSwipe(translation: CGFloat) {
+        if translation > .minDragTranslationForSwipe && selectedTab > 0 {
+            selectedTab -= 1
+        } else  if translation < -.minDragTranslationForSwipe && selectedTab < numTabs - 1 {
+            selectedTab += 1
+        }
+    }
+}
